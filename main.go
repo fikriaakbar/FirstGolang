6c5eb62fec11661490c9add5ea8a5895fadfbab7
unknown <fikriakbarhsb@gmail.com>
2020-09-01 22:34:13 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Article Structure
+type Article struct {
+	ID      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+//Articles ArrayArticle
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", homePage)
+	r.HandleFunc("/asd", returnAllArticles)
+	r.HandleFunc("/efg/{id}", returnSingleArticle)
+	r.HandleFunc("/hij", createNewArticle).Methods("POST")
+	r.HandleFunc("/klm/{id}", deleteArticle).Methods("DELETE")
+	http.ListenAndServe(":9000", r)
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	// Loop over all of our Articles
+	// if the article.Id equals the key we pass in
+	// return the article encoded as JSON
+	for _, article := range Articles {
+		fmt.Println(article)
+		if article.ID == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var data Article
+	fmt.Println(string(reqBody))
+	json.Unmarshal(reqBody, &data)
+	// update our global Articles array to include
+	// our new Article
+	Articles = append(Articles, data)
+
+	json.NewEncoder(w).Encode(data)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.ID == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+func main() {
+	Articles = []Article{
+		Article{ID: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{ID: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	fmt.Println("Fikri")
+	handleRequests()
+}
